ec373503d0f8fa551e1de00ae1244e771e2a4bdd
root <root@localhost.localdomain>
2016-05-12 23:05:11 -0400

    Adding some scripts to make building the tutorial pages easier.

---
+++ tutorials/wifi_script.php
@@ -0,0 +1,12 @@
+<?php
+    define ("NORMALIZE", "<link href='../css/normalize.css' rel='stylesheet'>");
+    define ("BOOTSTRAP", "<link href='../css/bootstrap.min.css' rel='stylesheet'>");
+    define ("STYLE", "<link href='../css/style.css' rel='stylesheet'>");
+    define("TITLE", "Netbeans and MAMP Setup | David Mendez");
+    include('php/head.php');
+?>
+   <div class='container' id="wrapper">
+
+
+<?php include('php/footer.php'); ?>
+Use this like this: make_bootstrap3_template > bootstrap3_template.html
