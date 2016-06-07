02dd50726335aaf4e9411ebc6e3c2f2f735c2e5b
David Mendez <mendezdavey@gmail.com>
2016-06-06 21:11:19 -0500

    Added LAMP on ubuntu linux installation web page.

--- how_to.php
+++ how_to.php
@@ -13,10 +13,10 @@ include('php/head.php');
           <p>In this link you will find help on how to build your very own web server.</p>
           <h3><a href="tutorials/netbeans_php_tutorial.php">Develop Websites Faster with Netbeans and MAMP web server.</a></h3>
           <p>In this link you will find help on setting up Netbeans and MAMP web server.</p>
-          <h3><a href="tutorials/install_flux.php">Install Flux on Centos7 and other gnu/linux os'"</a></h3>
+          <h3><a href="tutorials/install_flux.php">Install Flux on Centos7 and other gnu/linux os.</a></h3>
      	  <p>How to install flux from the terminal.</p>
-	  <h3><a href="tutorial/wifi_script.php">An example of a bash script to connect to wifi.</a></h3>
-	  <p>How to write a bash script to connect to wifi</p>
+	      <h3><a href="tutorials/lamp_ubuntulinux_install.php">How to install LAMP Server on Ubuntu Linux</a></h3>
+	      <p>How to install LAMP server on the terminal.</p>
       </div>
            
 
---
+++ tutorials/lamp_ubuntulinux_install.php
@@ -0,0 +1,58 @@
+<?php
+    define ("NORMALIZE", "<link href='../css/normalize.css' rel='stylesheet'>");
+    define ("BOOTSTRAP", "<link href='../css/bootstrap.min.css' rel='stylesheet'>");
+    define ("STYLE", "<link href='../css/style.css' rel='stylesheet'>");
+    define("TITLE", "Install Apache2 on Ubuntu Linux | David Mendez");
+    include('php/head.php');
+?>
+   <div class='container' id="wrapper">
+     <h1 id="intro">Install <a href='https://en.wikipedia.org/wiki/LAMP_%28software_bundle%29'>LAMP Server</a> on Ubuntu Linux.</h1>
+     <h2>Introduction</h2>
+     <p>Making in websites in HTML is good, but I have noticed that
+       there is allot of code repetition. To solve this issue I have begun using
+       more php in my web pages. A great way to test your php is to setup a local
+       web server on your computer. This tutorial will teach you how to set up a
+       <a href='https://en.wikipedia.org/wiki/LAMP_%28software_bundle%29'>LAMP Server</a> in ubuntu linux.</p>
+
+     <h2 id="step1">Install Apache2 on Ubuntu Linux</h2>
+     <p>According to <a href='https://en.wikipedia.org/wiki/Apache_HTTP_Server'>Wikipedia</a>: The Apache HTTP Server,
+       colloquially called Apache (/əˈpætʃiː/ ə-PA-chee),
+       is the world's most used web server software.
+       Originally based on the NCSA HTTPd server,
+       development of Apache began in early 1995 after work on the NCSA code stalled.
+       Apache played a key role in the initial growth of the World Wide Web,[5]
+       quickly overtaking NCSA HTTPd as the dominant HTTP server,
+       and has remained most popular since April 1996. In 2009,
+       it became the first web server software to serve more than 100 million websites.[6] </p>
+      <p>a) Run the commands:</p>
+      <pre>
+	<code class='code-box'>sudo apt-get update</code>
+      </pre>
+      <p>b) Install apache2</p>
+      <pre>
+	<code class='code-box'>sudo apt-get install apache2</code>
+      </pre>
+      <p>c) Install mysql database</p>
+       <pre>
+	<code class='code-box'>sudo apt-get install mysql</code>
+       </pre>
+       <p>d) Install php</p>
+       <pre>
+	<code class='code-box'>sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt</code>
+       </pre>
+       
+       <h2 id="step2">Running Apache2 Service</h2>
+       <p>a) Start apache2 service</p>
+       <pre>
+	<code class='code-box'>sudo systemctl start apache2</code>
+       </pre>
+       <p>b) Status apache2 service</p>
+       <pre>
+	<code class='code-box'>sudo systemctl status apache2</code>
+       </pre>
+       <p>c) Stop apache2 service</p>
+       <pre>
+	<code class='code-box'>sudo systemctl stop apache2</code>
+       </pre>       
+<?php include('php/footer.php'); ?>
+
