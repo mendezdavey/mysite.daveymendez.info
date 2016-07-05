640150d753f6458888c8407a01e3950aacbc7f62
mendezdavey <mendezdavey@gmail.com>
2016-07-04 19:07:07 -0500

    Some changes to the site

--- php/head.php
+++ php/head.php
@@ -25,6 +25,27 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+<script>
+function showRSS(str) {
+  if (str.length==0) {
+    document.getElementById("rssOutput").innerHTML="";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      document.getElementById("rssOutput").innerHTML=xmlhttp.responseText;
+    }
+  }
+  xmlhttp.open("GET","getrss.php?q="+str,true);
+  xmlhttp.send();
+}
+</script>
   </head>
   <body>
   <?php include('menu.php'); ?>
--- tutorials/lamp_ubuntulinux_install.php
+++ tutorials/lamp_ubuntulinux_install.php
@@ -2,47 +2,60 @@
     define ("NORMALIZE", "<link href='../css/normalize.css' rel='stylesheet'>");
     define ("BOOTSTRAP", "<link href='../css/bootstrap.min.css' rel='stylesheet'>");
     define ("STYLE", "<link href='../css/style.css' rel='stylesheet'>");
-    define("TITLE", "Install Apache2 on Ubuntu Linux | David Mendez");
+    define("TITLE", "Install LAMP Server on Ubuntu Linux");
     include('php/head.php');
 ?>
    <div class='container' id="wrapper">
-     <h4>Last Modified: June 7, 2016</h4>
-     <h1 id="intro">Install <a href='https://en.wikipedia.org/wiki/LAMP_%28software_bundle%29'>LAMP Server</a> on Ubuntu Linux.</h1>
+        
+<?php
+        /*
+        $t = date("H");
+
+        if ($t < "10") {
+            echo "<h4>Good Morning!</h4>" ;
+        } elseif ($t < "20") {
+            echo "Have a good day!";
+        } else {
+            echo "Have a good night!";
+        }
+        */
+?>
+       
+<?php
+/*
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+*/
+?>
+     <h4>Last Modified: June 30, 2016</h4>
+     <h1><?php echo TITLE; ?></h1>
+     <img src="../includes/img/lamp-mysql-php-apache1.png" style="max-width:50%;height:auto" >   
      <h2>Introduction</h2>
-     <p>Making a websites in HTML is good, but I have noticed that
-       there is allot of code repetition. To solve this issue I have begun using
-       more php in my web pages. A great way to test your php is to setup a local
+     <p>A great way to test your php is to setup a local
        web server on your computer. This tutorial will teach you how to set up a
        <a href='https://en.wikipedia.org/wiki/LAMP_%28software_bundle%29'>LAMP Server</a> in ubuntu linux.</p>
-
-     <h2 id="step1">Install Apache2 on Ubuntu Linux</h2>
-     <p>According to <a href='https://en.wikipedia.org/wiki/Apache_HTTP_Server'>Wikipedia</a>: The Apache HTTP Server,
-       colloquially called Apache (/əˈpætʃiː/ ə-PA-chee),
-       is the world's most used web server software.
-       Originally based on the NCSA HTTPd server,
-       development of Apache began in early 1995 after work on the NCSA code stalled.
-       Apache played a key role in the initial growth of the World Wide Web,[5]
-       quickly overtaking NCSA HTTPd as the dominant HTTP server,
-       and has remained most popular since April 1996. In 2009,
-       it became the first web server software to serve more than 100 million websites.[6] </p>
-      <p>a) Run the commands:</p>
+      <h2 id='step1'>Step 1: Run the Following Commands</h2>
+      <p>a) download the package lists and update them to their newest version.</p>
       <div class="black-box">
 	<code>sudo apt-get update</code>
      </div>
       <p>b) Install apache2</p>
      <div class="black-box">
 	<code class='code-box'>sudo apt-get install apache2</code>
-      </div>
-      <p>c) Install mysql database</p>
+     </div>
+    
+      <p>a) Install mysql database</p>
       <div class="black-box"> 
-	<code class='code-box'>sudo apt-get install mysql</code>
+	<code class='code-box'>sudo apt-get install mysql-server</code>
       </div>
-       <p>d) Install php</p>
+       <p>c) Install php</p>
        <div class="black-box">  
-	<code>sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt</code>
+	<code>sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt php-mysql</code>
       </div>
        
-       <h2 id="step2">Running Apache2 Service</h2>
+       <h2 id="step2">Step 2: Start/Stop Apache Service</h2>
        <p>a) Start apache2 service</p>
       <div class="black-box"> 
 	<code class='code-box'>sudo systemctl start apache2</code>
